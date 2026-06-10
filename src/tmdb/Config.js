eafbd73c6544556ed37fead364bc2f81ab1ad845
fvoltess <fedevoltes@gmail.com>
2026-06-10 15:25:47 -0300

    .gitignore update

---
+++ src/tmdb/Config.js
@@ -0,0 +1,3 @@
+export const TMDB_API_KEY = "b14bb0109adb851f41b72f2e04e5cd5c"
+export const TMDB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie";
+export const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w185";
